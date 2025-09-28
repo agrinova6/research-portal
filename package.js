013c775f7e996fc27d71df01e2555331bec46476
agrinova6 <agrisaarthitm@gmail.com>
2025-09-28 22:14:56 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "rescuevault-proxy-api",
+  "version": "1.0.0",
+  "main": "server.js",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "@supabase/supabase-js": "^2.58.0",
+    "cors": "^2.8.5",
+    "dotenv": "^17.2.2",
+    "express": "^4.21.2",
+    "multer": "^2.0.2"
+  }
+}
